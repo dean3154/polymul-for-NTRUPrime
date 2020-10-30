f7ed59563f070d3dc2a29637c5c765fa077e99c7
dean3154 <50229236+dean3154@users.noreply.github.com>
2020-10-31 01:02:25 +0800

    Add files via upload

--- 4591_761/polymul_761x761_modp.c
+++ 4591_761/polymul_761x761_modp.c
@@ -1,18 +1,12 @@
 #include <stdint.h>
 
-extern void fft17(void*f, void* fpad);
-extern void ntt17_rader(void* f, void* fpad);
-extern void fft17_byte(void* g, void* gpad);
-extern void ntt17_rader_byte(void* g, void* gpad);
-extern void byteToShort(void* gg, void* g);
-extern void fft9(void* fpad);
-extern void polymul_10x10_153(void* fpad, void* gpad);
-extern void polymul_10x10_153_mr(void* fpad, void* gpad);
-extern void ifft9(void* fpad);
-extern void ifft17(void* fpad, void* h);
-extern void ifft17_mr(void* fpad, void* h);
-extern void intt17_rader_mr(void* fpad, void* h);
-extern void mod_reduce(void* h, void* hpad);
+extern void ntt17_rader(int16_t* f, int16_t* fpad);
+extern void fft9(int16_t* fpad);
+extern void byteToShort(int16_t* gg, int8_t* g);
+extern void polymul_10x10_153_mr(int16_t* fpad, int16_t* gpad);
+extern void ifft9(int16_t* fpad);
+extern void intt17_rader_mr(int16_t* fpad, int16_t* h);
+extern void mod_reduce(int16_t* h, int16_t* hpad);
 // input  : int16_t f[761], int8_t g[761]
 // output  : int16_t h[761]
 // h = f * g mod x^761 -x -1 mod 4591
